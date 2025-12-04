e83db37df2b2e26f83040e7884b8664e7b8a9a56
baranwang <me@baran.wang>
2025-12-04 15:58:43 +0800

    Add Stremio addons configuration to manifest route
    - Introduced a new configuration object for Stremio addons, including issuer and signature properties.
    - This addition enhances the manifest route by integrating support for Stremio addons, improving overall functionality.

--- app/routes/manifest.json/index.ts
+++ app/routes/manifest.json/index.ts
@@ -20,5 +20,10 @@ export default createRoute(async (c) => {
     ],
     catalogs,
     // idPrefixes,
+    stremioAddonsConfig: {
+      issuer: "https://stremio-addons.net",
+      signature:
+        "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..Tx-U1fvAKi92Yl883DyjUw.jMq-g6l86Pf4zRl6Nuj5wg-WbQGfHnK1jCt6g_IxJnwkZdnS00iVUEP4g4ORUgNZgo_DjWGHpL2yz36UMn1ZHqSDtKpC0mh2Gm1nM3LHAUkEB3yFag5qPPLx6FufymYM.iQggztH740IgCNgzqktlyw",
+    },
   } satisfies Manifest);
 });
