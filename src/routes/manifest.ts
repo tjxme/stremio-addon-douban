fffe6460e51dce55bc935e17db860c024b7aee63
baranwang <me@baran.wang>
2025-12-30 17:02:31 +0800

    feat: 提升用户限速并根据用户代理动态调整清单资源

--- src/routes/manifest.ts
+++ src/routes/manifest.ts
@@ -3,6 +3,7 @@ import { type Env, Hono } from "hono";
 import pkg from "@/../package.json" with { type: "json" };
 import { getCatalogs } from "@/libs/catalog";
 import { encodeConfig, getConfig } from "@/libs/config";
+import { isForwardUserAgent } from "@/libs/utils";
 import { idPrefixes } from "./meta";
 
 export const manifestRoute = new Hono<Env>();
@@ -15,8 +16,13 @@ manifestRoute.get("/", async (c) => {
   }
 
   const config = await getConfig(c.env, configId);
-
   const catalogs = await getCatalogs(config);
+  const isInForward = isForwardUserAgent(c);
+
+  const resources: Manifest["resources"] = ["catalog"];
+  if (!isInForward) {
+    resources.push("meta");
+  }
   return c.json({
     id: `${pkg.name}.${configId}`,
     version: pkg.version,
@@ -24,7 +30,7 @@ manifestRoute.get("/", async (c) => {
     description: pkg.description,
     logo: "https://img1.doubanio.com/f/frodo/144e6fb7d96701944e7dbb1a9bad51bdb1debe29/pics/app/logo.png",
     types: ["movie", "series"],
-    resources: ["catalog", "meta"],
+    resources,
     catalogs,
     idPrefixes,
     behaviorHints: {
